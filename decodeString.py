ee0a01f9742b51c2f511495bc46b0431b7cc014d
Elvin Uthuppan <elvinutheman@gmail.com>
2019-05-01 09:57:04 -0400

    Working Solution

--- decodeString.py
+++ decodeString.py
@@ -5,19 +5,16 @@ def decodeString(input):
     i = 0
     while i < len(input):
         if input[i] != ']':
-            if ord(input[i]) >= 97 and ord(input[i]) <= 122:
+            if (ord(input[i]) >= 97 and ord(input[i]) <= 122) or input[i] == '[':
                 stack1.append(input[i])
             #Then this is a number
             else:
-                print(i)
                 digit = ''
                 while input[i] != '[':
                     digit += input[i]
                     i += 1
-                
-                print(i)
-                #print(digit)
-                stack1.append(int(digit))
+                stack1.append(digit)
+                stack1.append(input[i])
         else:
             tmp = ''
             while stack1[len(stack1)-1] != '[':
@@ -25,15 +22,14 @@ def decodeString(input):
             stack1.pop()
             mult = int(stack1.pop())
             newtmp = repeattmp(tmp, mult)
-            print(newtmp)
             stack1.append(newtmp)
         
         i += 1
     output = ''
-    print(stack1)
     while stack1:
         output = stack1.pop() + output
     return output
+
 def repeattmp(string, mult):
     output = ''
     for i in range(mult):
